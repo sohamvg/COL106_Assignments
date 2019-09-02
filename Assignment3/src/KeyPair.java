ee63c6f954c387474e8d3a76bfef5f61cc3c095a
sohamvg <sohamvg11@gmail.com>
2019-09-03 00:59:00 +0530

    assign3

---
+++ Assignment3/src/KeyPair.java
@@ -0,0 +1,16 @@
+public class KeyPair {
+    String first, second;
+
+    public KeyPair(String first, String second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public String getSecond() {
+        return second;
+    }
+}
---
+++ Assignment3/src/Student.java
@@ -0,0 +1,37 @@
+public class Student implements Student_ {
+
+    String fname, lname, hostel, department, cgpa;
+
+    public Student(String fname, String lname, String hostel, String department, String cgpa) {
+        this.fname = fname;
+        this.lname = lname;
+        this.hostel = hostel;
+        this.department = department;
+        this.cgpa = cgpa;
+    }
+
+    @Override
+    public String fname() {
+        return fname;
+    }
+
+    @Override
+    public String lname() {
+        return lname;
+    }
+
+    @Override
+    public String hostel() {
+        return hostel;
+    }
+
+    @Override
+    public String department() {
+        return department;
+    }
+
+    @Override
+    public String cgpa() {
+        return cgpa;
+    }
+}
